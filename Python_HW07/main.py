6601be190746a03ee6ca6e4a9118affaca23f851
Vlad-Tam <tamashenyavlad2004@gmail.com>
2024-05-28 11:06:02 +0300

    HW07 updated

--- Python_HW07/main.py
+++ Python_HW07/main.py
@@ -1,49 +1,17 @@
-def inches_to_cm(inches: float) -> float:
-    return inches * 2.54
-
-
-def cm_to_inches(cm: float) -> float:
-    return cm / 2.54
-
-
-def miles_to_km(miles: float) -> float:
-    return miles * 1.609
-
-
-def km_to_miles(km: float) -> float:
-    return km / 1.609
-
-
-def pounds_to_kg(pounds: float) -> float:
-    return pounds * 0.454
-
-
-def kg_to_pounds(kg: float) -> float:
-    return kg / 0.454
-
-
-def ounces_to_grams(ounces: float) -> float:
-    return ounces * 28.35
-
-
-def grams_to_ounces(grams: float) -> float:
-    return grams / 28.35
-
-
-def gallons_to_liters(gallons: float) -> float:
-    return gallons * 3.785
-
-
-def liters_to_gallons(liters: float) -> float:
-    return liters / 3.785
-
-
-def pints_to_liters(pints: float) -> float:
-    return pints * 0.473
-
-
-def liters_to_pints(liters: float) -> float:
-    return liters / 0.473
+functions = {
+    1: lambda inches: inches * 2.54,
+    2: lambda cm: cm / 2.54,
+    3: lambda miles: miles * 1.609,
+    4: lambda km: km / 1.609,
+    5: lambda pounds: pounds * 0.454,
+    6: lambda kg: kg / 0.454,
+    7: lambda ounces: ounces * 28.35,
+    8: lambda grams: grams / 28.35,
+    9: lambda gallons: gallons * 3.785,
+    10: lambda liters: liters / 3.785,
+    11: lambda pints: pints * 0.473,
+    12: lambda liters: liters / 0.473
+}
 
 
 def output_menu():
@@ -63,44 +31,17 @@ def output_menu():
     print("0. Exit")
 
 
-def choice_function(choice: int, value: float):
-    if choice == '1':
-        print(f"{value} inches = {inches_to_cm(value)} cm")
-    elif choice == '2':
-        print(f"{value} cm = {cm_to_inches(value)} inches")
-    elif choice == '3':
-        print(f"{value} miles = {miles_to_km(value)} km")
-    elif choice == '4':
-        print(f"{value} km = {km_to_miles(value)} miles")
-    elif choice == '5':
-        print(f"{value} pounds = {pounds_to_kg(value)} kg")
-    elif choice == '6':
-        print(f"{value} kg = {kg_to_pounds(value)} pounds")
-    elif choice == '7':
-        print(f"{value} ounces = {ounces_to_grams(value)} g")
-    elif choice == '8':
-        print(f"{value} g = {grams_to_ounces(value)} ounces")
-    elif choice == '9':
-        print(f"{value} gallons = {gallons_to_liters(value)} l")
-    elif choice == '10':
-        print(f"{value} l = {liters_to_gallons(value)} gallons")
-    elif choice == '11':
-        print(f"{value} pints = {pints_to_liters(value)} l")
-    elif choice == '12':
-        print(f"{value} l = {liters_to_pints(value)} pints")
-    else:
-        print("Invalid choice. Please try again.")
-
-
 def main():
     while True:
         output_menu()
-        choice = input("Enter the number of the action: ")
-        if choice == '0':
+        choice = int(input("Enter the number of the action: "))
+        if choice == 0:
             break
-        choice_function(choice, value=float(input("Enter the value: ")))
+        value = float(input("Enter the value: "))
+        func = functions.get(choice)
+        if func:
+            print(func(value))
 
 
 if __name__ == '__main__':
     main()
-
